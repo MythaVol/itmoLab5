30553dc23ab0a380d63d400907c8010ae4da7180
mytha <mytha_vol@outlook.com>
2024-03-19 11:07:02 +0300

    2 commit

--- src/main/java/MusicBands/Coordinates.java
+++ src/main/java/MusicBands/Coordinates.java
@@ -4,4 +4,9 @@ public class Coordinates {
     private float x;
     private float y; //Значение поля должно быть больше -455
 
+    public Coordinates(int x, int y){
+        this.x=x;
+        this.y = y;
+    }
+
 }
