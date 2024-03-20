4353f6d8998e5945b7cb9ebb39522ecb8118e18f
mytha <mytha_vol@outlook.com>
2024-03-20 16:28:43 +0300

    3 commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
-public class Main {
-    public static void main(String[] args) {
+import MusicBands.MusicBand;
 
+
+public class Main {
+    public static void main(String[] args){
 
     }
 }
--- src/main/java/MusicBands/MusicBand.java
+++ src/main/java/MusicBands/MusicBand.java
@@ -1,5 +1,10 @@
 package MusicBands;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.beans.ConstructorProperties;
+
+@JsonSerialize
 public class MusicBand {
     private Long id; //Поле не может быть null, Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
     private String name; //Поле не может быть null, Строка не может быть пустой
@@ -8,4 +13,16 @@ public class MusicBand {
     private long numberOfParticipants; //Значение поля должно быть больше 0
     private MusicGenre genre; //Поле не может быть null
     private Studio studio; //Поле не может быть null
+
+    public MusicBand(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
